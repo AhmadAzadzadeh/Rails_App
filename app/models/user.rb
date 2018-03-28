f136ccfa081cabaa5925a8fa9faa7736df446a46
AhmadAzadzadeh <a.azadzadeh.aa14@gmail.com>
2018-03-28 20:09:47 +0430

    implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,12 @@ class User < ApplicationRecord
         uniqueness: { case_sensitive: false }
     # validates(:email, presence: true)
     has_secure_password
-    validates :password, presence: true ,length: { maximum: 6 }
+    validates :password, presence: true ,length: { minimum: 6 }
+
+    # Returns the hash digest of the given string.
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    # root "controller_name#action_name"
   root 'static_pages#home'
@@ -15,6 +17,12 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new"
 
   post "/signup", to: "users#create"
+
+  get "/login", to: "sessions#new"
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
 
   resources :users
 end
